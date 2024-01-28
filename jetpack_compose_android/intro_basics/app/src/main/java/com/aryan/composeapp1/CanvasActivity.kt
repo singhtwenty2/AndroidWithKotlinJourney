fe2e3da958017413ca12327b7f74e2c3b8265c13
Aryan Singh <125189374+mrysg@users.noreply.github.com>
2024-01-28 13:30:30 +0530

    Math operations in Compose

--- jetpack_compose_android/intro_basics/app/src/main/java/com/aryan/composeapp1/CanvasActivity.kt
+++ jetpack_compose_android/intro_basics/app/src/main/java/com/aryan/composeapp1/CanvasActivity.kt
@@ -3,17 +3,27 @@ package com.aryan.composeapp1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
@@ -25,7 +35,15 @@ class CanvasActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CircularProgressBar(
+                    percentage = 0.9f,
+                    maxNumber = 100
+                )
+            }
         }
     }
 }
@@ -41,7 +59,43 @@ fun CircularProgressBar(
     animDuration: Int = 1000,
     animDelay: Int = 0
 ) {
-    var animPlayed by remember{
+    var animPlayed by remember {
         mutableStateOf(false)
+    }
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (animPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        ), label = "Circular Animation"
+    )
+    LaunchedEffect(key1 = true) {
+        animPlayed = true
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas( // Canvas is used to draw our own custom views.
+            modifier = Modifier
+                .size(radius * 2f)
+        ) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(
+                    width = strokeWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
+            )
+        }
+        Text(
+            text = (currentPercentage.value * maxNumber).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
