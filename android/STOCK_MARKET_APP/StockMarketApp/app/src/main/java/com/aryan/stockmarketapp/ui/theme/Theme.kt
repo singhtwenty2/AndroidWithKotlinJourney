f6fd5a2a14ee1d1da689f4faea53e42357e88215
Aryan Singh <125189374+mrysg@users.noreply.github.com>
2024-01-29 21:53:26 +0530

    Minor Changes

--- android/STOCK_MARKET_APP/StockMarketApp/app/src/main/java/com/aryan/stockmarketapp/ui/theme/Theme.kt
+++ android/STOCK_MARKET_APP/StockMarketApp/app/src/main/java/com/aryan/stockmarketapp/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.plcoding.stockmarketapp.ui.theme
+package com.aryan.stockmarketapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
--- android/STOCK_MARKET_APP/StockMarketApp/app/src/test/java/com/aryan/stockmarketapp/ExampleUnitTest.kt
+++ android/STOCK_MARKET_APP/StockMarketApp/app/src/test/java/com/aryan/stockmarketapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.stockmarketapp
+package com.aryan.stockmarketapp
 
 import org.junit.Test
 
